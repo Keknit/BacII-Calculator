0630f23082f688a808d3377aa08dfeff9ce1626f
vatana chhorn <47716174+VatanaChhorn@users.noreply.github.com>
2020-10-12 16:12:43 +0700

    initial commit

---
+++ BacII-Calculator/BacII_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BacII_CalculatorApp.swift
+//  BacII-Calculator
+//
+//  Created by Vatana Chhorn on 12/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct BacII_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BacII-Calculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BacII-Calculator
+//
+//  Created by Vatana Chhorn on 12/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
